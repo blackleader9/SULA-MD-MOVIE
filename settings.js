e92f8013157a9d1b162c6ba02e32b0e83806709d
blackleader9 <arpubgpubg868@gmail.com>
2025-07-20 09:50:27 +0530

    diwu.pdate settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=53N3iYRA#WanO4ghpmVrHjzpCmLSIG8c31x8M732g-NdrI-8DVXU' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=czcW1LoZ#OgFGNNyQnxVz7jLiM0LMjLe63nfprguTyJrvn0DCFkA' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS === undefined ?"true" : process.env.AUTO_REACT_STATUS,
