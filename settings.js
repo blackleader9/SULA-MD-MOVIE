b00096fc2eaec6e65accf121d30ebeb8c2e936ae
blackleader9 <arpubgpubg868@gmail.com>
2025-07-22 18:43:42 +0530

    pasi ayya  settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=pqUShZCR#PLqrYf-NC5yhQ0HFFbgrMdYYn-XS-uy-n3m2AO8Ax8Q' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=Ajd32DIS#6ixoOGKtJmj-upSVDctDVcMqOEtV2L6hU3ALoZA7jKQ' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS === undefined ?"true" : process.env.AUTO_REACT_STATUS,
