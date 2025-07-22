f7e35be94bbbe229f8453cc5f61a52ceeb22e86d
blackleader9 <arpubgpubg868@gmail.com>
2025-07-22 21:21:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=Ajd32DIS#6ixoOGKtJmj-upSVDctDVcMqOEtV2L6hU3ALoZA7jKQ' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=o3MTGAQC#pwmhKWSE_IDhGW_moUAJl9-FIy9OIuvPkp2Tjnmeo3o' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS === undefined ?"true" : process.env.AUTO_REACT_STATUS,
