3fa398cddde62c02147a472835b8c7689680460c
blackleader9 <arpubgpubg868@gmail.com>
2025-07-25 17:57:03 +0530

    pasi settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=p3F0mRDY#jylmIvcBrViQ8NG3x0SQI1WNGr_hStY6LDCp6n8lNjs' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=kqM3CLDC#8G8wzhPFQNK1hcerQGyvY8bgE5YEbJ3lSiY4CtgnqCM' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS === undefined ?"true" : process.env.AUTO_REACT_STATUS,
